76542ae6e475ae4fd463b48b8aa1a4d08442537a
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2024-04-28 11:45:02 -0600

    Removed harpoon, added barbar and indent guides

--- lua/eklipsed/lazy.lua
+++ lua/eklipsed/lazy.lua
@@ -8,26 +8,27 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate"
 	},
-	{
-  		'projekt0n/github-nvim-theme',
-  		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  		priority = 1000, -- make sure to load this before all the other start plugins
-  		config = function()
-    			require('github-theme').setup({
-      				-- ...
-    			})
-
-    			vim.cmd('colorscheme github_dark')
-  		end,
-	},
 	'nvim-treesitter/playground',
-	'ThePrimeagen/harpoon',
 	'mbbill/undotree',
 	'tpope/vim-fugitive',
 	--- Uncomment these if you want to manage LSP servers from neovim
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
-	
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+	{'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		--init = function() vim.g.barbar_auto_setup = false end,
+		opts = {
+				-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+				-- animation = true,
+				-- insert_at_start = true,
+				-- …etc.
+		}
+	},
+	{'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'neovim/nvim-lspconfig'},
 	{'hrsh7th/cmp-nvim-lsp'},
